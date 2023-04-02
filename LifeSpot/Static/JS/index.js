0db4c9deb53b71b377c18fba56dbf2d572b1713d
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-04-02 16:09:40 +0300

    проверка на возраст с сохранением данных в объект, функция в index.js

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,21 +1,26 @@
-﻿let userInfo=new Map();
-let UserAgent = window.navigator.userAgent;
-userInfo.set('UserAgent', UserAgent);
-age = prompt("Введите свой возраст.", "");
-if (age >= 18) { alert("Приветствуем на LifeSpot! " + new Date().toLocaleString()); }
-else {
-    alert("Данный сайт не предназначен для лиц младше 18 лет!");
-    window.location.href = "http://www.google.com";
-};
-userInfo.set('Age', age);
-let timeOfSesionStart = new Date().toLocaleString()
-userInfo.set('TimeOfSesionStart',timeOfSesionStart);
+﻿//функция фильтрации контента
+function sessionInfo() {
+    let userInfo = new Map();
+    let UserAgent = window.navigator.userAgent;
+    userInfo.set('UserAgent', UserAgent);
+    age = prompt("Введите свой возраст.", "");
+    if (age >= 18) {
+        alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+    } else {
+        alert("Данный сайт не предназначен для лиц младше 18 лет!");
+        window.location.href = "http://www.google.com";
+    }
+    ;
+    userInfo.set('Age', age);
+    let timeOfSesionStart = new Date().toLocaleString()
+    userInfo.set('TimeOfSesionStart', timeOfSesionStart);
 
 // Вывод в консоль
-for (let result of userInfo){
-    console.log(result)
+    for (let result of userInfo) {
+        console.log(result)
+    }
 }
-//возраст, время начала сессии, строку User Agent
+
 
 //функция фильтрации контента
 function filtarionContent() {
