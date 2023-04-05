3bb56b2f8299da5758e3a61e856347d17f1baa80
DESKTOP-BBDMNJN\Dima <ryabchikk@rambler.ru>
2023-04-05 20:19:19 +0300

    добавил вызов функций по нажатию на кнопку

--- LifeSpot/Static/JS/about.js
+++ LifeSpot/Static/JS/about.js
@@ -1,6 +1,13 @@
-
+let session =  new Map();
 
 function sessionInfo() {
-    let age = prompt("Пожалуйста, введите ваш возраст?");
-    console.log(age);
+    session.set("age", prompt("Пожалуйста, введите ваш возраст/"));
+    session.set("comment", prompt("Введите коментарий"));
+    console.log(session.get("age"));
+    console.log(session.get("comment"));
+}
+
+const postComment = sesion => {
+    console.log(sesion.get("age"));
+    console.log(sesion.get("comment"));
 }
